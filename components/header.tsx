709f824f316c44d0d321f5964ed4a1da9c02a07e
alanagoyal <alanaan@gmail.com>
2025-03-14 13:21:36 -0700

    add nav items to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,24 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function Header() {
   const { theme, setTheme } = useTheme();
   return (
     <div className="bg-background border-b">
-    <div className="h-16 px-3 py-2 flex justify-between items-center">
-      <button 
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-        className="font-bold font-display text-xl cursor-pointer"
-      >
-        BOOKLIST
-      </button>
+      <div className="h-16 px-3 py-2 flex justify-between items-center">
+        <div className="flex items-center gap-6">
+          <Link
+            href="/"
+            className="font-bold font-display text-xl cursor-pointer"
+          >
+            BOOKLIST
+          </Link>
+          <Link href="/about">About</Link>
+          <Link href="/contribute">Contribute</Link>
+        </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
