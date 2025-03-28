ea50e779be3fd97e5a1c9519f617925447255d98
alanagoyal <alanaan@gmail.com>
2025-03-27 15:30:44 -1000

    fix title

--- app/roulette/page.tsx
+++ app/roulette/page.tsx
@@ -8,7 +8,7 @@ export default async function RoulettePage() {
     return (
       <div className="h-full p-4 bg-background text-text overflow-y-auto">
         <div className="max-w-2xl mx-auto">
-          <h1 className="text-xl font-base mb-6">Book Roulette</h1>
+          <h1 className="text-xl font-base mb-6">Roulette</h1>
           <div className="text-text">No books found. Please try again later.</div>
         </div>
       </div>
@@ -18,7 +18,7 @@ export default async function RoulettePage() {
   return (
     <div className="h-full p-4 bg-background text-text overflow-y-auto">
       <div className="max-w-2xl mx-auto">
-        <h1 className="text-xl font-base mb-6">Book Roulette</h1>
+        <h1 className="text-xl font-base mb-6">Roulette</h1>
         <Roulette initialBook={initialBook} />
       </div>
     </div>
