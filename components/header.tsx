5c13c4c6444c49b49b3848254b54c45757cc2aa5
alanagoyal <alanaan@gmail.com>
2025-03-31 15:12:05 -0700

    change order of nav

--- components/header.tsx
+++ components/header.tsx
@@ -23,10 +23,10 @@ function HeaderContent() {
         >
           BOOKLIST
         </Link>
-        <Link href={getHrefWithParams("/about")}>About</Link>
         <Link href={getHrefWithParams("/graph")}>Graph</Link>
         <Link href={getHrefWithParams("/roulette")}>Roulette</Link>
         <Link href={getHrefWithParams("/contribute")}>Contribute</Link>
+        <Link href={getHrefWithParams("/about")}>About</Link>
       </div>
       <ThemeToggle />
     </div>
