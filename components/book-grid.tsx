37a9972d8e0059ff0755e0a53bf1ce748de50c77
alanagoyal <alanaan@gmail.com>
2025-04-17 19:56:17 -0700

    use button instead of custom tooltip

--- components/book-grid.tsx
+++ components/book-grid.tsx
@@ -2,7 +2,6 @@
 
 import { Fragment, useState, useCallback, useMemo } from "react";
 import { DataGrid } from "@/components/grid";
-import { PercentileTooltip } from "@/components/percentile-tooltip";
 import { FormattedBook, EnhancedBook } from "@/types";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -52,11 +51,15 @@ function RecommenderCell({ original }: { original: EnhancedBook }) {
             </span>
           )}
         </span>
-        <PercentileTooltip
-          percentile={original._percentile}
-          open={tooltipOpen}
-          setOpen={setTooltipOpen}
-        />
+        <button 
+          title={`${original._percentile}th percentile of all recommendations`}
+          className="inline-flex items-center justify-center rounded-full text-text/70 md:hover:text-text transition-colors duration-200 cursor-help"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clipRule="evenodd" />
+          </svg>
+        </button>
       </span>
     </div>
   );
