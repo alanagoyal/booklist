e9a1d39bb684dc4f27983dbf5c73de1d2f517133
alanagoyal <alanaan@gmail.com>
2025-04-22 12:54:39 -0700

    fix leading height for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <div className="h-full p-4 bg-background text-text overflow-y-auto">
       <div className="max-w-3xl mx-auto p-4">
-        <h1 className="text-[28px] font-bold mt-8 mb-4">About</h1>
+        <h1 className="text-[28px] leading-none font-bold mt-8 mb-4">About</h1>
         <About />
       </div>
     </div>
