ecc189fcc48baa567446dcb3b05987dbbcf2122b
alanagoyal <alanaan@gmail.com>
2025-04-22 12:52:20 -0700

    add padding to pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import About from "@/components/about";
 export default function AboutPage() {
   return (
     <div className="h-full p-4 bg-background text-text overflow-y-auto">
-      <div className="max-w-3xl mx-auto">
+      <div className="max-w-3xl mx-auto p-4">
         <h1 className="text-[28px] font-bold mt-8 mb-4">About</h1>
         <About />
       </div>
