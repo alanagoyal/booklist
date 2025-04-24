354f86215b89fa0577de9ec4ce452f56edc24d70
alanagoyal <alanaan@gmail.com>
2025-04-24 09:44:00 -0700

    fix error

--- components/contribute.tsx
+++ components/contribute.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { useState, useEffect, useCallback } from 'react'
+import { useState } from 'react'
 import { Plus, X, ExternalLink, Wand2 } from 'lucide-react'
-import { useDebounce } from '@/lib/hooks/use-debounce'
 
 interface BookRecommendation {
   title: string
