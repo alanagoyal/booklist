560aa440d00c08b169b4d6814860955a1827ae48
alanagoyal <alanaan@gmail.com>
2025-04-24 12:08:34 -0700

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { BookList } from "@/components/book-list";
-import type { EssentialBook, ExtendedBook, FormattedBook, FormattedRecommender } from "@/types";
+import type { EssentialBook, ExtendedBook, FormattedRecommender } from "@/types";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then(r => r.json());
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
 
 export default function Home() {
-  const { data: essentialBooks, error: essentialError } = useSWR<EssentialBook[]>(
+  const { data: essentialBooks } = useSWR<EssentialBook[]>(
     `${baseUrl}/data/books-essential.json`,
     fetcher
   );
-  const { data: recommenders, error: recommendersError } = useSWR<FormattedRecommender[]>(
+  const { data: recommenders } = useSWR<FormattedRecommender[]>(
     `${baseUrl}/data/recommenders.json`,
     fetcher
   );
