e8f96f285493fe1d0f5584609246e44891fe34e6
alanagoyal <alanaan@gmail.com>
2025-05-18 09:06:48 -0700

    clean up icon and add stop propagation to button

--- components/recommender-grid.tsx
+++ components/recommender-grid.tsx
@@ -6,6 +6,7 @@ import { FormattedRecommender } from "@/types";
 import { useRouter, useSearchParams } from "next/navigation";
 import { truncateText } from "@/utils/text";
 import { formatPercentile } from "@/utils/format";
+import { InfoIcon } from './icons'
 
 interface RecommenderGridProps {
   data: FormattedRecommender[];
@@ -62,19 +63,11 @@ function RecommendationCell({
           <button
             title={formatPercentile(original.recommendation_percentile) + " percentile"}
             className="inline-flex items-center justify-center rounded-full text-text/70 md:hover:text-text transition-colors duration-200 cursor-help shrink-0"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <InfoIcon />
           </button>
         )}
       </span>
