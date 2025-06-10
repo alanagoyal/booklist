60a30c63ddd0aba7853ca816525cad4aeb7af879
alanagoyal <alanaan@gmail.com>
2025-06-10 07:59:17 -0700

    fix site config url

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
     title: "Booklist",
     description: "The most frequently recommended books on the internet",
-    url: process.env.NEXT_PUBLIC_VERCEL_URL,
+    url: "https://basecase.vc/booklist",
   };
